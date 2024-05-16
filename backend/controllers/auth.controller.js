91d3d9fa48c125dbf7bfc98e3c1e1d57a4ffd18f
robeertosv <resvolkava@gmail.com>
2024-05-16 15:15:18 +0200

    Login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,21 +1,22 @@
 import User from "../models/user.model.js"
 import bcrypt from 'bcrypt'
+import generateTokenAndCookie from "../utils/cookieGen.js";
 
 export const createAccount = async (req, res) => {
     try {
-        const {username, fullname, password, confirmPassword, phone, profilePic} = req.body;
+        const { username, fullname, password, confirmPassword, phone, profilePic } = req.body;
 
         const existeElUsuario = await User.findOne({ username })
 
-        if(existeElUsuario) return res.status(400).json({ error: "Ya existe ese usuario" })
-        
-        if(password != confirmPassword || password.length < 8) return res.status(400).json({ error: "La contraseña es muy corta" })
+        if (existeElUsuario) return res.status(400).json({ error: "Ya existe ese usuario" })
+
+        if (password != confirmPassword || password.length < 8) return res.status(400).json({ error: "La contraseña es muy corta" })
 
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(password, salt);
 
         const newUser = new User({
-            username, 
+            username,
             fullname,
             password: hashedPass,
             phone,
@@ -24,16 +25,31 @@ export const createAccount = async (req, res) => {
 
         await newUser.save();
 
-        return res.status(200).send("OK")
+        return res.status(201).send("Created")
     } catch (error) {
         console.log(error)
         return res.status(500).json({ error: "Error al crear el usuario: " + error.message })
-        
+
     }
 }
 
 export const login = async (req, res) => {
-    
+    try {
+        const { username, password } = req.body;
+
+        const existeElUser = await User.findOne({ username });
+        if (!existeElUser) return res.status(404).json({ error: "No se encontró al usuario" })
+
+        const correctPass = await bcrypt.compare(password, user?.password || "");
+
+        if (!correctPass) return res.status(403).send({ error: "La contraseña no es correcta" })
+
+        const cookie = generateTokenAndCookie(user._id, res);
+
+        return res.status(200).send("OK");
+    } catch (error) {
+        return res.status(500).json({ error: "Error al hacer el login" })
+    }
 }
 
 export const logout = async (req, res) => {
---
+++ backend/utils/cookieGen.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+const generateTokenAndCookie = async (userId, res) => {
+    const token = jwt.sign({userId}, process.env.API_KEY, { expiresIn: '30d' })
+
+    res.cookie("sessionToken", token, {
+        maxAge: 30 * 24 * 3600,
+        sameSite: "strict",
+        httpOnly: true
+    })
+}
+
+export default generateTokenAndCookie;
