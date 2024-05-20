68bada9f975255f5f7050780d55a6ebc8cebc306
robeertosv <resvolkava@gmail.com>
2024-05-20 12:37:55 +0200

    Not Found  Page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,6 +1,13 @@
+import '../styles/NotFound.scss'
+
 const NotFound = () => {
+  function goHome() { window.location.replace("/") }
   return (
-    <div>NotFound</div>
+    <div>
+      <h1>404</h1>
+      <h2>Not Found</h2>
+      <button onClick={goHome}>GO HOME</button>
+    </div>
   )
 }
 
